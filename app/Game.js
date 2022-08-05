258a9f917afc4b7cfb8dc1a2094f826cb0ad66a3
thewilkupl <thewilku@o2.pl>
2022-08-05 21:57:24 +0200

    implementation of guessing letters

--- app/Game.js
+++ app/Game.js
@@ -1,12 +1,70 @@
+import { Quote } from "./Quote.js";
+
 class Game {
+
+  quotes = [
+    {
+      text: "pan tadeusz",
+      category: "Utwór literacki",
+    },
+    {
+      text: "janko muzykant",
+      category: "Utwór literacki",
+    },
+    {
+      text: "akademia pana kleksa",
+      category: "Film",
+    },
+    {
+      text: "ogniem i mieczem",
+      category: "Film",
+    },
+  ];
+
   constructor({ lettersWrapper, categoryWrapper, wordWrapper, outputWrapper }) {
     this.lettersWrapper = lettersWrapper;
     this.categoryWrapper = categoryWrapper;
     this.wordWrapper = wordWrapper;
     this.outputWrapper = outputWrapper;
+
+    const { text, category, } = this.quotes[Math.floor(Math.random() * this.quotes.length)];
+    this.categoryWrapper.innerHTML = category;
+    this.quote = new Quote(text);
+  }
+
+  guess(letter, event){
+    event.target.disabled = true;
+    this.quote.guess(letter);
+    this.drawQuote();
+  }
+
+  drawLetters(){
+    for (let i = 0; i < 26; i++) {
+      const label = (i + 10).toString(36);
+      const button = document.createElement('button');
+      button.innerHTML = label;
+      button.addEventListener('click', (event) => this.guess(label, event))
+      this.lettersWrapper.appendChild(button);
+    }
+  }
+
+  drawQuote(){
+    const content = this.quote.getContent();
+    this.wordWrapper.innerHTML = content;
   }
 
   start() {
-    console.log("Let's play the game!");
+    this.drawLetters();
+    this.drawQuote();
   }
+
+
 }
+
+const game = new Game({
+  lettersWrapper: document.getElementById('letters'),
+  categoryWrapper: document.getElementById('category'),
+  wordWrapper: document.getElementById('word'),
+  outputWrapper: document.getElementById('output'),
+});
+game.start();
---
+++ app/Quote.js
@@ -0,0 +1,27 @@
+export class Quote {
+    constructor(text) {
+        this.text = text;
+        this.quessed = [];
+    }
+
+    getContent(){
+        let content = '';
+        for (const char of this.text){
+            if(char === ' ' || this.quessed.includes(char)){
+                content += char;
+            } else {
+                content += '_';
+            }
+        }
+        return content;
+    }
+
+    guess(letter) {
+        if(!this.text.includes(letter)){
+            return false
+        }
+        this.quessed.push(letter);
+        return true;
+    }
+
+}
