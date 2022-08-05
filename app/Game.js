ab2d8248c180b35c127ced2196d20ec6c2c32910
thewilkupl <thewilku@o2.pl>
2022-08-05 22:14:21 +0200

    main game logic

--- app/Game.js
+++ app/Game.js
@@ -1,23 +1,25 @@
-import { Quote } from "./Quote.js";
+import { Quote } from './Quote.js';
 
 class Game {
+  currentStep = 0;
+  lastStep = 7;
 
   quotes = [
     {
-      text: "pan tadeusz",
-      category: "Utwór literacki",
+      text: 'pan tadeusz',
+      category: 'Utwór literacki',
     },
     {
-      text: "janko muzykant",
-      category: "Utwór literacki",
+      text: 'janko muzykant',
+      category: 'Utwór literacki',
     },
     {
-      text: "akademia pana kleksa",
-      category: "Film",
+      text: 'akademia pana kleksa',
+      category: 'Film',
     },
     {
-      text: "ogniem i mieczem",
-      category: "Film",
+      text: 'ogniem i mieczem',
+      category: 'Film',
     },
   ];
 
@@ -27,38 +29,57 @@ class Game {
     this.wordWrapper = wordWrapper;
     this.outputWrapper = outputWrapper;
 
-    const { text, category, } = this.quotes[Math.floor(Math.random() * this.quotes.length)];
+    const { text, category } = this.quotes[Math.floor(Math.random() * this.quotes.length)];
     this.categoryWrapper.innerHTML = category;
     this.quote = new Quote(text);
   }
 
-  guess(letter, event){
+  guess(letter, event) {
     event.target.disabled = true;
-    this.quote.guess(letter);
-    this.drawQuote();
+    if (this.quote.guess(letter)) {
+      this.drawQuote();
+    } else {
+      this.currentStep++;
+      document.getElementsByClassName('step')[this.currentStep].style.opacity = 1;
+      if(this.currentStep === this.lastStep){
+        this.loosing();
+      }
+    }
   }
 
-  drawLetters(){
+  drawLetters() {
     for (let i = 0; i < 26; i++) {
       const label = (i + 10).toString(36);
       const button = document.createElement('button');
       button.innerHTML = label;
-      button.addEventListener('click', (event) => this.guess(label, event))
+      button.addEventListener('click', (event) => this.guess(label, event));
       this.lettersWrapper.appendChild(button);
     }
   }
 
-  drawQuote(){
+  drawQuote() {
     const content = this.quote.getContent();
     this.wordWrapper.innerHTML = content;
+    if(!content.includes('_')){
+      this.winning()
+    }
   }
 
   start() {
+    document.getElementsByClassName('step')[this.currentStep].style.opacity = 1;
     this.drawLetters();
     this.drawQuote();
   }
 
+  winning() {
+    this.wordWrapper.innerHTML = 'GRATULACJE WYGRYWASZ! KONIEC GRY!';
+    this.lettersWrapper.innerHTML = '';
+  }
 
+  loosing() {
+    this.wordWrapper.innerHTML = 'PRZEGRAŁEŚ! KONIEC GRY!';
+    this.lettersWrapper.innerHTML = '';
+  }
 }
 
 const game = new Game({
--- app/Quote.js
+++ app/Quote.js
@@ -1,27 +1,26 @@
 export class Quote {
-    constructor(text) {
-        this.text = text;
-        this.quessed = [];
-    }
+  constructor(text) {
+    this.text = text;
+    this.quessed = [];
+  }
 
-    getContent(){
-        let content = '';
-        for (const char of this.text){
-            if(char === ' ' || this.quessed.includes(char)){
-                content += char;
-            } else {
-                content += '_';
-            }
-        }
-        return content;
+  getContent() {
+    let content = '';
+    for (const char of this.text) {
+      if (char === ' ' || this.quessed.includes(char)) {
+        content += char;
+      } else {
+        content += '_';
+      }
     }
+    return content;
+  }
 
-    guess(letter) {
-        if(!this.text.includes(letter)){
-            return false
-        }
-        this.quessed.push(letter);
-        return true;
+  guess(letter) {
+    if (!this.text.includes(letter)) {
+      return false;
     }
-
+    this.quessed.push(letter);
+    return true;
+  }
 }
