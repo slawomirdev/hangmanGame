20ad884e46e0371815665405d2386f036b2f4a95
thewilkupl <thewilku@o2.pl>
2022-08-05 19:32:52 +0200

    initial state

---
+++ app/Game.js
@@ -0,0 +1,17 @@
+class Game {
+  constructor({
+    lettersWrapper,
+    categoryWrapper,
+    wordWrapper,
+    outputWrapper,
+  }) {
+      this.lettersWrapper = lettersWrapper;
+      this.categoryWrapper = categoryWrapper;
+      this.wordWrapper = wordWrapper;
+      this.outputWrapper = outputWrapper;
+  }
+
+  start() {
+    console.log("Let's play the game!");
+  }
+}
